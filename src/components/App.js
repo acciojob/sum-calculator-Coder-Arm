d54e0b1d6aebb26ba74a8878ed3a4add2ffc426a
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-29 09:41:33 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [sum , setSum] = useState(0);
+  
+  function handleSum(e){
+    const input = Number(e.target.value)
+    setSum((prevSum) => prevSum+input)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="number" onChange={(e) => handleSum(e)}/>
+        <div>Sum : {sum}</div>
     </div>
   )
 }
