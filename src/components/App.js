9d690b628b34df07456939c6d8a51fa46cc5ec76
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-29 09:48:07 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <div>
       <h1>Sum Calculator</h1>
         <input type="number" onChange={(e) => handleSum(e)}/>
-        <p>Sum : {sum}</p>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
